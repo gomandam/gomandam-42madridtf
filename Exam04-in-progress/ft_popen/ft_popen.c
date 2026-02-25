4c5941d44ade3c0fe8c21179cf0bf14eb69b9a1b
g01100001 <gomandam@student.42madrid.com>
2026-02-25 23:21:49 +0100

    [add] ft_popen.c

---
+++ Exam04-in-progress/ft_popen/ft_popen.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_popen.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 17:23:44 by gomandam          #+#    #+#             */
+/*   Updated: 2026/02/21 01:06:47 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// POPEN; pipe stream to or from a process. PIPE; unidirectional. 
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_popen(char *file, char *const argv[], char type)
+{
+	int	fd[2];
+	int	r = type == 'r';
+	int	pid = 0;
+
+	if (!file || !argv || (type ^ 'r' && type ^ 'w') || pipe(fd) || (pid = fork()) < 0)
+		return (close(fd[0]), close(fd[1]), -1);
+	if (!pid)
+	{
+		dup2(fd[r], r);
+		close(fd[0]);
+		close(fd[1]);
+		execvp(file, argv);
+		exit(1);
+	}
+	close(fd[r]);
+	return (fd[!r]);
+}
+
+
+/*
+Assignment name  : ft_popen
+Expected files   : ft_popen.c
+Allowed functions: pipe, fork, dup2, execvp, close, exit
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int ft_popen(const char *file, char *const argv[], char type);
+
+The function must launch the executable file with the arguments argv
+(using execvp).
+
+If type is 'r' the function must return a file descriptor
+connected to the output of the command.
+
+If type is 'w' the function must return a file descriptor connected to the
+input of the command.
+
+In case of error or invalid parameter the function must return -1.
+
+For example, the function could be used like that:
+
+int main()
+{
+    int  fd;
+    char *line;
+
+    fd = ft_popen("ls", (char *const []){"ls", NULL}, 'r');
+    while ((line = get_next_line(fd)))
+        ft_putstr(line);
+    return (0);
+}
+
+int	main() {
+	int	fd = ft_popen("ls", (char *const []){"ls", NULL}, 'r');
+	dup2(fd, 0);
+	fd = ft_popen("grep", (char *const []){"grep", "c", NULL}, 'r');
+	char	*line;
+	while ((line = get_next_line(fd)))
+		printf("%s", line);
+}
+
+Hints:
+Do not leak file descriptors!
+This exercise is inspired by the libc's popen().
+*/
