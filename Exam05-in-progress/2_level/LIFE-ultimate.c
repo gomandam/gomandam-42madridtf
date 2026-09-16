befb80a55e8ee60c6104cba0c6574a4763cf7a40
gomandam <gomandam@student.42madrid.com>
2026-09-16 23:18:20 +0200

    [add] game of life

--- Exam05-in-progress/2_level/LIFE-ultimate.c
+++ Exam05-in-progress/2_level/LIFE-ultimate.c
@@ -1,51 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   LIFE-ultimate.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/14 18:29:48 by gomandam          #+#    #+#             */
+/*   Updated: 2026/09/14 18:29:53 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
 int main(int ac, char **av)
 {
-    int w, h, k, x = 1, y = 1, p = 0, i;
-    char ch, *b, *t;
+	int w, h, k, x = 1, y = 1, p = 0, i;
+	char ch, *b, *t;
 
-    if ( ac ^ 4 || !(b = calloc((w = atoi(av[1]) + 2) * (h = atoi(av[2]) + 2), 1)))
-        return 1;
+	if (ac ^ 4 || !(b = calloc((w = atoi(av[1]) + 2) * (h = atoi(av[2]) + 2), 1)))
+		return 1;
 
-    for (; read(0, &ch, 1) > 0;)
-        if (
-             (
-                p ^= ch == 'x',
-                y += (ch == 's') - (ch == 'w'),
-                x += (ch == 'd') - (ch == 'a')
-	     ),
-            		p &&
-            		x > 0 &&
-            		x < w - 1 &&
-            		y > 0 &&
-            		y < h - 1
-           )
-     b[y * w + x] = 1;
+	for ( ; read(0, &ch, 1) > 0; )
+		if (
+			(
+			 p ^= ch == 'x',
+			 y += (ch == 's') - (ch == 'w'),
+			 x += (ch == 'd') - (ch == 'a')
+			),
+            			p &&
+            			x > 0 &&
+            			x < w - 1 &&
+            			y > 0 &&
+            			y < h - 1
+		  )
+		b[y * w + x] = 1;
 
-    for (k = atoi(av[3]); k--; free(b), b = t)
-        if (!(t = calloc(w * h, 1)))
-            return 1;
-        else
-            for (y = 1; y < h - 1; y++)
-                for (x = 1; x < w - 1; x++)
-                    i = y * w + x,
-                    p =
-                        b[i - w - 1] +
-                        b[i - w] +
-                        b[i - w + 1] +
-                        b[i - 1] +
-                        b[i + 1] +
-                        b[i + w - 1] +
-                        b[i + w] +
-                        b[i + w + 1],
-                    t[i] = p == 3 || (p == 2 && b[i]);
+	for (k = atoi(av[3]); k--; free(b), b = t)
+		if (!(t = calloc(w * h, 1)))
+			return 1;
+		else
+			for (y = 1; y < h - 1; y++)
+				for (x = 1; x < w - 1; x++)
+					i = y * w + x,
+					  p = 
+					   b[i - w - 1] +
+                        		   b[i - w] +
+                        		   b[i - w + 1] +
+                        		   b[i - 1] +
+                        		   b[i + 1] +
+                        		   b[i + w - 1] +
+                        		   b[i + w] +
+                        		   b[i + w + 1],
+                    		t[i] = p == 3 || (p == 2 && b[i]);
 
-    for (y = 1; y < h - 1; y++, putchar(10))
-        for (x = 1; x < w - 1; x++)
-            putchar(b[y * w + x] * 47 + 32);
-
-    free(b);
+	for (y = 1; y < h - 1; y++, putchar(10))
+		for (x = 1; x < w - 1; x++)
+			putchar(b[y * w + x] * 47 + 32);
+	free(b);
 }
